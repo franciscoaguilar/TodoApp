038431e9ef5f5f2a06cf3c12d1a2524482742d44
francisco <0667034@my.scccd.edu>
2018-06-25 13:58:25 -0700

    delete, edit, add all work properly

--- js/scripts.js
+++ js/scripts.js
@@ -1,36 +1,51 @@
 
-const myBtn = document.getElementById('btn'); //create a variable to be able to acess the button
-
+const myAddBtn = document.getElementById('btn'); //create a variable to be able to acess the button
+// const deleteBtn = document.getElementsByTagName('button');
 let todos = [];
 // function Task(task){
 //   this.task = task;
 // }
- myBtn.addEventListener('click', e =>{
+ myAddBtn.addEventListener('click', e =>{
   let createElementLi = document.createElement("LI"); //create a new list element (just the bullet)
   let usersInput = document.getElementById('usersInput').value; //gets whatever tasked the user typed in
   let newTextNode = document.createTextNode(usersInput); //create a new text node with whatever task the user typed in, so that i can later add to the list elemet
+// let elementsInputbox = document.createElement('input');
+// elementsInputbox.className = "inputBox";
   let deleteButtonElement = document.createElement('button'); //creates a button, ill be making this a delete butoon
+  deleteButtonElement.className = "delBtn";
+   let editButtonElement = document.createElement('button');// creates a button, ill be making this a edit butoon
+editButtonElement.className = "editBtn";
 
-deleteButtonElement.className = "deleteClass";
-  let editButtonElement = document.createElement('button');// creates a button, ill be making this a edit butoon
- // let newUsersInput = new Task(usersInput);
+
  document.getElementById("list").appendChild(createElementLi); //adds nand displays the combined element and task to the current list
-
-
+  // createElementLi.appendChild(elementsInputbox);
   createElementLi.appendChild(newTextNode); //adds the the new task to the newlist element(bullet)
-  createElementLi.appendChild(deleteButtonElement); //adds
+  createElementLi.appendChild(deleteButtonElement); //adds delete btn to the li
   createElementLi.appendChild(editButtonElement);
-todos.push(usersInput); //adds the new input to an array
+  todos.push(usersInput); //adds the new input to an array
+
+  deleteButtonElement.addEventListener('click', e =>{
+    const removeElement= e.target.parentNode;
+    console.log(removeElement);
+     removeElement.parentNode.removeChild(removeElement);
+ })
+ editButtonElement.addEventListener('click', e =>{
+   const editElement = e.target.parentNode;
+   let ul = editElement.parentNode;
+  let   promptEdit = prompt('edit:' );
+  let newChildInput = document.createTextNode(promptEdit);
+  editElement.replaceChild(newChildInput, editElement.firstChild);
+})
 document.forms.taskform.reset();
+
 
 console.log(todos);
 
 })
 
-let deleteBtn = document.getElementsByClassName( 'deleteClass' );
 
-deleteBtn.addEventListener('click', e => {
-  console.log(e.target.value)
-   const removeElement = e.target.textContent;
-    e.target.value = removeElement.parentNode.removeChild();
-})
+
+
+// deletetask (event){
+//   let node = eve
+// }
