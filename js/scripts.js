79a585bcdd5622a0e1d5fda2face64cd8ff0e942
francisco <0667034@my.scccd.edu>
2018-06-27 12:03:20 -0700

    icons inserted

--- js/scripts.js
+++ js/scripts.js
@@ -6,46 +6,67 @@ let todos = [];
 //   this.task = task;
 // }
  myAddBtn.addEventListener('click', e =>{
+
   let createElementLi = document.createElement("LI"); //create a new list element (just the bullet)
   let usersInput = document.getElementById('usersInput').value; //gets whatever tasked the user typed in
   let newTextNode = document.createTextNode(usersInput); //create a new text node with whatever task the user typed in, so that i can later add to the list elemet
 // let elementsInputbox = document.createElement('input');
 // elementsInputbox.className = "inputBox";
   let deleteButtonElement = document.createElement('button'); //creates a button, ill be making this a delete butoon
-  deleteButtonElement.className = "delBtn";
-   let editButtonElement = document.createElement('button');// creates a button, ill be making this a edit butoon
-editButtonElement.className = "editBtn";
+    deleteButtonElement.className = "fas fa-trash";
+  let editButtonElement = document.createElement('button');// creates a button, ill be making this a edit butoon
+    editButtonElement.className = "fas fa-edit";
+  let checkboxElement = document.createElement('input');
+  checkboxElement.setAttribute("type", "checkbox");
+    checkboxElement.className = "checkboxBtn";
 
 
+if (usersInput=='' ){ //edge casing make shure the users adds something
+  alert('HEY! STOP! you have too add something');
+}
+else{
  document.getElementById("list").appendChild(createElementLi); //adds nand displays the combined element and task to the current list
   // createElementLi.appendChild(elementsInputbox);
   createElementLi.appendChild(newTextNode); //adds the the new task to the newlist element(bullet)
   createElementLi.appendChild(deleteButtonElement); //adds delete btn to the li
   createElementLi.appendChild(editButtonElement);
+  createElementLi.appendChild(checkboxElement);
   todos.push(usersInput); //adds the new input to an array
-
+if(deleteButtonElement=='click'){
+  alert('delete?')
+}
+else{
   deleteButtonElement.addEventListener('click', e =>{
     const removeElement= e.target.parentNode;
     console.log(removeElement);
      removeElement.parentNode.removeChild(removeElement);
  })
+}
  editButtonElement.addEventListener('click', e =>{
-   const editElement = e.target.parentNode;
-   let ul = editElement.parentNode;
-  let   promptEdit = prompt('edit:' );
-  let newChildInput = document.createTextNode(promptEdit);
-  editElement.replaceChild(newChildInput, editElement.firstChild);
+   const editElement = e.target.parentNode
+   console.log(editElement);
+  let newEditElememt = editElement.setAttribute("contenteditable", "True");
+  // let editInputBox = document.createElement('input')
+  // let edit = editInputBox.appendChild(editElement);
+  // editElement.setAttribute("contenteditable", "True");
+    // let   promptEdit = prompt('edit:' );
+    // let newInput = document.createTextNode(promptEdit);
+    editElement.replaceChild(newEditElememt, editElement.firstElementNode);
+    // console.log(todosArray); //adds the new input to an array
+})
+
+
+checkboxElement.addEventListener('click', e =>{
+
+const checkBox = e.target.parentNode; // allow me to target the parents node of the checkbox
+ let createCompletedLiElement = document.createElement('li'); ///create another LI element for the done list
+ document.getElementById('doneList').appendChild(createCompletedLiElement);// acess the done list and add the li element that ive created on the above step
+ createCompletedLiElement.append(checkBox.firstChild);// appends the users input (newTextNode) to the newly created li
+checkBox.remove(); //removes the entire li element since i target the parentNode when i declared checkbox.
 })
 document.forms.taskform.reset();
 
 
 console.log(todos);
-
+}
 })
-
-
-
-
-// deletetask (event){
-//   let node = eve
-// }
